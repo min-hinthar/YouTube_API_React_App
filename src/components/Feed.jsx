be1604543de6291ffd8abebdebae8a9f516dec2b
min-hinthar <102896221+min-hinthar@users.noreply.github.com>
2023-01-23 16:52:29 -0800

    Feed updateded with Sidebar component and styling

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import { useState, useEffect } from "react";
+import { Box, Stack, Typography } from "@mui/material";
 
 const Feed = () => {
   return (
-    <div>Feed</div>
+    <Stack
+      sx={{ flexDirection: { sx: 'column', md: 'row' }}}
+    >
+      <Box
+        sx={{ height: { sx: 'auto', md: '92vh' }, borderRight: '2px solid #A51C30', px: {sx: 0, md: 2}}}
+      >
+        Sidebar
+
+        <Typography 
+          className="copyright"
+            variant='body2'
+            sx={{ mt: 1.5, color: '#fff' }}
+          >
+          Copyright © 2023 Min.K 
+        </Typography>
+      </Box>
+    </Stack>
   )
 }
 
