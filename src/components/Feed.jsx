e364a5bfabee03a419181192a42b5b611a4ab56e
min-hinthar <102896221+min-hinthar@users.noreply.github.com>
2023-01-23 17:47:10 -0800

    Video.jsx component created and added to Feed

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import { Box, Stack, Typography } from "@mui/material";
-import Sidebar from "./Sidebar";
+
+import { Sidebar, Videos } from './index';
 
 const Feed = () => {
   return (
@@ -8,10 +9,10 @@ const Feed = () => {
       sx={{ flexDirection: { sx: 'column', md: 'row' }}}
     >
       <Box
-        sx={{ height: { sx: 'auto', md: '92vh' }, borderRight: '2px solid #A51C30', px: {sx: 0, md: 2}}}
+        sx={{ height: { sx: 'auto', md: '92vh' }, borderRight: '2px solid #A51C30', px: {sx: 0, md: 2}, p:1 }}
       >
         <Sidebar />
-
+      {/* Copyright */}
         <Typography 
           className="copyright"
             variant='body2'
@@ -19,6 +20,33 @@ const Feed = () => {
           >
           Copyright © 2023 Min.K 
         </Typography>
+      </Box>
+    {/* Videos Feed */}
+      <Box 
+        p={2}
+        sx={{
+          overflow: 'auto',
+          height: '90vh',
+          flex: 2,
+        }}
+      >
+        <Typography 
+          variant="h4"
+          fontWeight='bold'
+          mb={2}
+          sx={{
+            color: 'white'
+          }}
+        >
+          NEW
+          <span 
+            style={{ color: '#A51C30'}}
+          >
+            Videos
+          </span>
+        </Typography>
+      {/* VIDEOS */}
+        <Videos />
       </Box>
     </Stack>
   )
