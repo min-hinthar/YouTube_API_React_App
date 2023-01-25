adc4477df937d48c2daa04ba1bbacfd044cfb619
min-hinthar <102896221+min-hinthar@users.noreply.github.com>
2023-01-24 23:27:10 -0800

    Right side panel of related videos rendered in column

--- src/components/VideoDetail.jsx
+++ src/components/VideoDetail.jsx
@@ -2,20 +2,24 @@ import { useState, useEffect } from 'react';
 import { Link, useParams } from 'react-router-dom';
 import ReactPlayer from 'react-player';
 import { Typography, Box, Stack } from '@mui/material';
-import { Title, Verified } from '@mui/icons-material';
+import { Verified } from '@mui/icons-material';
 
-import { Video } from './';
+import { Videos } from './index';
 import { fetchFromAPI } from '../utils/fetchFromAPI';
 
 const VideoDetail = () => {
 
-  const [videoDetail, setVideoDetail] = useState(null)
+  const [videoDetail, setVideoDetail] = useState(null);
+  const [videos, setVideos] = useState(null)
 
   const { id } = useParams();
 
   useEffect(() => {
     fetchFromAPI(`videos?part=snippet,statistics&id=${id}`)
-      .then((data) => setVideoDetail(data.items[0]))
+      .then((data) => setVideoDetail(data.items[0]));
+
+    fetchFromAPI(`search?part=snippet&relatedToVideoId=${id}&type=video`)
+      .then((data) => setVideos(data.items));
   }, [id]);
 
   if (!videoDetail?.snippet) return 'Loading...';
@@ -78,6 +82,15 @@ const VideoDetail = () => {
               </Stack>
             </Stack>
           </Box>
+        </Box>
+      {/* Side Videos */}
+        <Box
+          px={2}
+          py={{ md: 1, xs: 5 }}
+          justifyContent='center'
+          alignItems='center'
+        >
+          <Videos videos={videos} direction='column'/>
         </Box>
       </Stack>
     </Box>
